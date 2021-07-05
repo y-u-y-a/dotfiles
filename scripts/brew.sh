83918ab44785758d0dc027edde5bd10ba9c3bef1
y-u-y-a <47471299+y-u-y-a@users.noreply.github.com>
2021-07-05 21:33:14 +0900

    不要ファイル削除

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+FILE_PATH=~/.bash_profile
+
 # <Homebrew> ====================================================
 echo 'Start Install Homebrew...'
 /bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)'
@@ -15,6 +17,16 @@ echo 'Start Install MySQL...'
 brew install mysql@5.6
 brew link mysql@5.6 --force # シンボリックを貼る
 
+# <Node> =======================================================
+echo 'Start Install Node.js...'
+brew install nodebrew # バージョン管理
+echo 'export PATH=$PATH:$HOME/.nodebrew/current/bin' >> FILE_PATH
+source FILE_PATH
+
+nodebrew install-binary stable # 最新の安定版をインストール
+
+npm install -g typescript
+
 # <Ruby> ========================================================
 echo 'Start Install Ruby...'
 brew install\
@@ -24,9 +36,9 @@ brew install\
 brew link readline --force # シンボリックを貼る
 RUBY_CONFIGURE_OPTS='--with-readline-dir=$(brew --prefix readline)' # readlineを通してRubyをインストール
 
-echo "eval '$(rbenv init -)'" >> ~/.bash_profile
-echo 'export PATH=$PATH:$HOME/.rbenv/shims:/usr/local/bin' >> ~/.bash_profile
-source ~/.bash_profile
+echo "eval '$(rbenv init -)'" >> FILE_PATH
+echo 'export PATH=$PATH:$HOME/.rbenv/shims:/usr/local/bin' >> FILE_PATH
+source FILE_PATH
 
 rbenv install --list
 read -p 'Rubyのバージョンを指定してください。' rubyVer < /dev/tty
@@ -39,26 +51,16 @@ echo 'Start Install Python...'
 brew install pyenv # バージョン毎にライブラリ管理
 brew install pipenv # プロジェクト毎にライブラリ管理
 
-echo "eval '$(pyenv init -)'" >> ~/.bash_profile
-echo 'export PYENV_ROOT=$HOME/.pyenv' >> ~/.bash_profile
-echo 'export PATH=$PATH:$PYENV_ROOT/bin' >> ~/.bash_profile
-source ~/.bash_profile
+echo "eval '$(pyenv init -)'" >> FILE_PATH
+echo 'export PYENV_ROOT=$HOME/.pyenv' >> FILE_PATH
+echo 'export PATH=$PATH:$PYENV_ROOT/bin' >> FILE_PATH
+source FILE_PATH
 
 pyenv install --list
 read -p 'Pythonのバージョンを指定してください。' pythonVer < /dev/tty
 pyenv install ${pythonVer}
 pyenv global ${pythonVer}
 pyenv rehash
-
-# <Node> =======================================================
-echo 'Start Install Node.js...'
-brew install nodebrew # バージョン管理
-echo 'export PATH=$PATH:$HOME/.nodebrew/current/bin' >> ~/.bash_profile
-source ~/.bash_profile
-
-nodebrew install-binary stable # 最新の安定版をインストール
-
-npm install -g typescript
 
 # <PHP> ===========================================================
 echo 'Start Install PHP...'
@@ -74,9 +76,9 @@ brew install php@7.4
 # # php-build内に環境変数を設定
 # PHP_BUILD_CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl)"
 
-# echo 'eval "$(phpenv init -)"' >> ~/.bash_profile
-# echo 'export PATH=$PATH:$HOME/.phpenv/bin' >> ~/.bash_profile
-# source ~/.bash_profile
+# echo 'eval "$(phpenv init -)"' >> FILE_PATH
+# echo 'export PATH=$PATH:$HOME/.phpenv/bin' >> FILE_PATH
+# source FILE_PATH
 
 # phpenv install ${phpVer}
 # phpenv global ${phpVer}
@@ -87,36 +89,37 @@ brew install php@7.4
 echo 'Start install Go...'
 brew install go
 
-echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
-echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bash_profile
-echo 'export GO111MODULE=on' >> ~/.bash_profile # Go Modulesを使用
-echo 'export GOPROJECT=$HOME/src/github.com/y-u-y-a' >> ~/.bash_profile
+echo 'export GOPATH=$HOME/go' >> FILE_PATH
+echo 'export PATH="$PATH:$GOPATH/bin"' >> FILE_PATH
+echo 'export GO111MODULE=on' >> FILE_PATH # Go Modulesを使用
+echo 'export GOPROJECT=$HOME/src/github.com/y-u-y-a' >> FILE_PATH
 
-source ~/.bash_profile
+source FILE_PATH
 
 
 # <GUIアプリ> =======================================================
 echo 'Start Install Apps...'
 
-brew cask install\
+brew install --cask\
     google-chrome\
     firefox\
-    slack\
     skype\
+    slack\
     visual-studio-code\
-    sourcetree\
+    postman\
     sequel-pro\
     github\
     docker\
-    docker-toolbox\
+    sourcetree\
+    aws-cli\
+    aws-vault\
+    google-cloud-sdk\
     google-backup-and-sync\
     chromedriver\
     heroku/brew/heroku\
-    google-cloud-sdk\
     ngrok\
     watch\
     bat\
-    postman\
     # kubectl\
     # minikube\
     # virtualbox\
